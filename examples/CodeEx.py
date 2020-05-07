51b4a29266f76ede14cc89fc3f8cde8b9751c753
suhan-jung <suhan.jung@gmail.com>
2020-05-07 17:45:25 +0900

    add exzmples

--- examples/CodeEx.py
+++ examples/CodeEx.py
@@ -1,13 +1,12 @@
 import win32com.client
- 
- 
+
 # 연결 여부 체크
 objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
 bConnect = objCpCybos.IsConnect
 if (bConnect == 0):
     print("PLUS가 정상적으로 연결되지 않음. ")
     exit()
- 
+
 # 종목코드 리스트 구하기
 objCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
 codeList = objCpCodeMgr.GetStockListByMarket(1) #거래소
